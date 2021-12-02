d9515fe6ceb49fefe901960b44cd373c35148ca1
riddhesh-mahajan <riddheshgp@gmail.com>
2021-12-02 15:30:49 +0530

    setup

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route, HashRouter } from "react-router-dom";
+import Home from './Home';
+
+
+export default function App() {
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </HashRouter>
+    )
+}
