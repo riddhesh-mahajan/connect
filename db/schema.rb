41c12abd4d21513266e4142f2f0d417875f52021
riddhesh-mahajan <riddheshgp@gmail.com>
2021-12-08 17:34:44 +0530

    fix: pg gem error

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_12_04_072621) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "followings", force: :cascade do |t|
     t.integer "user_id"
     t.integer "followed_user_id"
